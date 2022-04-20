257b9d4c3f0f8274954512009c60d69f91cee3f9
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-04-20 11:37:29 +0200

    Update TestCafe.js

--- tests/Blogifier.Tests/UI/TestCafe.js
+++ tests/Blogifier.Tests/UI/TestCafe.js
@@ -1,4 +1,4 @@
-import { Selector } from "testcafe"
+import { Selector, ClientFunction } from "testcafe"
 
 fixture("Login test")
 
@@ -21,3 +21,49 @@ test.page("http://localhost:5000/admin/register")("PasswordsDoesntMatch", async 
     await t.click("#app > div > form > button")
     await t.expect(await Selector(".validation-message").withText("Passwords do not match").exists).ok()
 })
+
+test.page("http://localhost:5000/admin/register")("ItWorks", async t => {
+    await t
+        .typeText("#registerEmail", "test@gmail.com")
+        .typeText("#registerName", "TestAdmin")
+        .typeText("#registerPassword", "123456")
+        .typeText("#registerConfirmPassword", "123456")
+        
+    await t.click("#app > div > form > button")
+    
+    const location = await ClientFunction(() => document.location.href)
+    await t.expect(location()).contains("/admin/login")
+})
+
+test.page("http://localhost:5000/admin/login")("EmailDoesntExist", async t => {
+    await t
+        .typeText("#loginEmail", "test99@gmail.com")
+        .typeText("#loginPassword", "123456")
+        
+    await t.click("#app > div > form > button")
+    
+    await t.expect(Selector(".account-message").exists).ok()
+})
+
+test.page("http://localhost:5000/admin/login")("PasswordDoesntExist", async t => {
+    await t
+        .typeText("#loginEmail", "test@gmail.com")
+        .typeText("#loginPassword", "654321")
+        
+    await t.click("#app > div > form > button")
+    
+    await t.expect(Selector(".account-message").exists).ok()
+})
+
+test.page("http://localhost:5000/admin/login")("RedirectToDashboard", async t => {
+    await t
+        .typeText("#loginEmail", "test@gmail.com")
+        .typeText("#loginPassword", "123456")
+        
+    await t.click("#app > div > form > button")
+    
+    const location = await ClientFunction(() => document.location.href)
+    await t.expect(location()).contains("/admin/")
+})
+
+
