acf020a14cea76d0b7343e53824db2cd7f03ac19
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-04-20 13:01:01 +0200

    Update TestCafe.js

--- testcafe/TestCafe.js
+++ testcafe/TestCafe.js
@@ -2,7 +2,7 @@ import { Selector, ClientFunction } from "testcafe"
 
 fixture("Login test")
 
-test.page("http://localhost:5000/admin/register")("RegisterWithNoInfo", async t => {
+test.page("http://185.51.76.36:9888/admin/register")("RegisterWithNoInfo", async t => {
     let validationExists = await Selector(".validation-message").exists
     await t.expect(validationExists).notOk()
     await t.click("#app > div > form > button")
@@ -11,7 +11,7 @@ test.page("http://localhost:5000/admin/register")("RegisterWithNoInfo", async t 
     await t.expect(validationExists).ok()
 })
 
-test.page("http://localhost:5000/admin/register")("PasswordsDoesntMatch", async t => {
+test.page("http://185.51.76.36:9888/admin/register")("PasswordsDoesntMatch", async t => {
     await t
         .typeText("#registerEmail", "test@gmail.com")
         .typeText("#registerName", "TestAdmin")
@@ -22,7 +22,7 @@ test.page("http://localhost:5000/admin/register")("PasswordsDoesntMatch", async 
     await t.expect(await Selector(".validation-message").withText("Passwords do not match").exists).ok()
 })
 
-test.page("http://localhost:5000/admin/register")("ItWorks", async t => {
+test.page("http://185.51.76.36:9888/admin/register")("ItWorks", async t => {
     await t
         .typeText("#registerEmail", "test@gmail.com")
         .typeText("#registerName", "TestAdmin")
@@ -35,7 +35,7 @@ test.page("http://localhost:5000/admin/register")("ItWorks", async t => {
     await t.expect(location()).contains("/admin/login")
 })
 
-test.page("http://localhost:5000/admin/login")("EmailDoesntExist", async t => {
+test.page("http://185.51.76.36:9888/admin/login")("EmailDoesntExist", async t => {
     await t
         .typeText("#loginEmail", "test99@gmail.com")
         .typeText("#loginPassword", "123456")
@@ -45,7 +45,7 @@ test.page("http://localhost:5000/admin/login")("EmailDoesntExist", async t => {
     await t.expect(Selector(".account-message").exists).ok()
 })
 
-test.page("http://localhost:5000/admin/login")("PasswordDoesntExist", async t => {
+test.page("http://185.51.76.36:9888/admin/login")("PasswordDoesntExist", async t => {
     await t
         .typeText("#loginEmail", "test@gmail.com")
         .typeText("#loginPassword", "654321")
@@ -55,7 +55,7 @@ test.page("http://localhost:5000/admin/login")("PasswordDoesntExist", async t =>
     await t.expect(Selector(".account-message").exists).ok()
 })
 
-test.page("http://localhost:5000/admin/login")("RedirectToDashboard", async t => {
+test.page("http://185.51.76.36:9888/admin/login")("RedirectToDashboard", async t => {
     await t
         .typeText("#loginEmail", "test@gmail.com")
         .typeText("#loginPassword", "123456")
