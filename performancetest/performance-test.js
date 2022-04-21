1db6cf538066c294a15ec46bfe8cfae0a6358414
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-04-21 14:15:38 +0200

    Update performance-test.js

--- performancetest/performance-test.js
+++ performancetest/performance-test.js
@@ -1,5 +1,7 @@
 import { sleep } from 'k6';
 import http from 'k6/http';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
 
 export const options = {
     duration: '10s',
@@ -12,5 +14,11 @@ export const options = {
 export default function () {
     http.get('http://185.51.76.36:9888/api/post/list/All/Post');
     sleep(3);
+}
+
+export function handleSummary(data) {
+    return {
+        "summary.html": htmlReport(data),
+    };
 }
 
